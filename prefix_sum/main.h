895dd02bd7c9b4c69fadf938355f0c0e5f0af537
YKH0725 <825362306@qq.com>
2025-09-11 17:23:18 +0800

    Add files via upload

---
+++ prefix_sum/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <hip/hip_runtime.h>
+#include <fstream>
+#include <cstdlib>
+
+extern "C" void solve(const int* input, int* output, int N);
+
+#endif 
